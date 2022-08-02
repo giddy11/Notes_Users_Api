3556cdacb9c9d780d67e7a6842e909dc5aa507c5
gideon <edoghotugiddy@gmail.com>
2022-08-02 16:13:12 +0100

    Created a database and added the service to my program.cs

--- CCSANoteApp.DB/SessionFactory.cs
+++ CCSANoteApp.DB/SessionFactory.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using CCSANoteApp.DB.Mappings;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using NHibernate.Tool.hbm2ddl;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +13,23 @@ namespace CCSANoteApp.DB
 {
     public class SessionFactory
     {
+        public SessionFactory()
+        {
+
+        }
+
+        public static ISessionFactory CreateSessionFactory()
+        {
+            return Fluently.Configure()
+                .Database(
+                    MsSqlConfiguration.MsSql2008
+                        .ConnectionString(
+                            @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\ADMIN\Desktop\Phase II\Asp.Net Api\Notes_Users\NoteAppDB.mdf;Integrated Security=True;Connect Timeout=30")
+                        .ShowSql()
+                )
+                .Mappings(m => m.FluentMappings.AddFromAssemblyOf<NoteMap>())
+                .ExposeConfiguration(cfg => new SchemaUpdate(cfg).Execute(false, true))
+                .BuildSessionFactory();
+        }
     }
 }
