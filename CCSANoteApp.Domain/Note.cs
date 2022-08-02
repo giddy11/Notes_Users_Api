18a4d6c915e1ff3177893eecd8c568832dc790c2
gideon <edoghotugiddy@gmail.com>
2022-08-02 15:42:42 +0100

    setting up the user,and note  models in the Domain with virtual to every props

--- CCSANoteApp.Domain/Note.cs
+++ CCSANoteApp.Domain/Note.cs
@@ -2,6 +2,17 @@
 {
     public class Note
     {
+        public Note()
+        {
+            Id = Guid.NewGuid();
+        }
 
+        public virtual Guid Id { get; set; }
+        public virtual User NoteCreator { get; set; }
+        public virtual string Title { get; set; }
+        public virtual string Content { get; set; }
+        public virtual GroupName Group { get; set; }
+        public virtual DateTime CreatedDate { get; set; } = DateTime.Now;
+        public virtual DateTime UpdatedDate { get; set; } = DateTime.Now;
     }
 }
--- CCSANoteApp.Domain/User.cs
+++ CCSANoteApp.Domain/User.cs
@@ -2,6 +2,15 @@
 {
     public class User
     {
+        public User()
+        {
+            Id = Guid.NewGuid();
+        }
 
+        public virtual Guid Id { get; set; }
+        public virtual string Username { get; set; }
+        public virtual string Email { get; set; }
+        public virtual string Password { get; set; }
+        public virtual List<Note> Notes { get; set; } = new();
     }
 }
