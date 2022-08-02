11a634b044cc8448c7b79212fe1d83580017bb88
gideon <edoghotugiddy@gmail.com>
2022-08-02 16:06:04 +0100

    setting up the notemap and usermap class

--- CCSANoteApp.DB/Mappings/NoteMap.cs
+++ CCSANoteApp.DB/Mappings/NoteMap.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CCSANoteApp.Domain;
+using FluentNHibernate.Mapping;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,17 @@ using System.Threading.Tasks;
 
 namespace CCSANoteApp.DB.Mappings
 {
-    public class NoteMap
+    public class NoteMap : ClassMap<Note>
     {
+        public NoteMap()
+        {
+            Id(note => note.Id);
+            Map(note => note.NoteCreator);
+            Map(note => note.Title);
+            Map(note => note.Content);
+            Map(note => note.Group);
+            Map(note => note.CreatedDate);
+            Map(note => note.UpdatedDate);
+        }
     }
 }
--- CCSANoteApp.DB/Mappings/UserMap.cs
+++ CCSANoteApp.DB/Mappings/UserMap.cs
@@ -1,6 +1,18 @@
-﻿namespace CCSANoteApp.DB.Mappings
+﻿using CCSANoteApp.Domain;
+using FluentNHibernate.Mapping;
+
+namespace CCSANoteApp.DB.Mappings
 {
-    public class UserMap
+    public class UserMap : ClassMap<User>
     {
+        public UserMap()
+        {
+            Table("Users");
+            Id(user => user.Id);
+            Map(user => user.Username);
+            Map(user => user.Password);
+            Map(user => user.Email);
+            HasMany(user => user.Notes);
+        }
     }
 }
